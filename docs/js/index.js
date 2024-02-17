50d88194eb5505736f9c4af42000708c4d60ce77
Deyvidas <devidbanyavichyus@yandex.ru>
2024-02-18 01:44:25 +0400

    Add header

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,1 +1,1 @@
-import{mobileNav}from"./modules/mobile-nav.js";mobileNav();import{tagline}from"./modules/tagline.js";tagline();
+import{tagline}from"./modules/tagline.js";tagline();
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,6 @@
 // Handle interactions with the menu button.
-import { mobileNav } from './modules/mobile-nav.js';
-mobileNav();
+// import { mobileNav } from './modules/mobile-nav.js';
+// mobileNav();
 
 // Handle interactions with the close tagline button.
 import { tagline } from './modules/tagline.js';
