20ff3c1f209e7a06a84370989216be38e87a54d9
Deyvidas <devidbanyavichyus@yandex.ru>
2024-02-20 19:02:30 +0400

    Add a section with categories.
    Disabled the option to select or drop images.
    Disabled the option to select text for links.
    The background image on the main part of the page changes height dynamically when the taglink is closed.

--- src/ts/modules/tagline.ts
+++ src/ts/modules/tagline.ts
@@ -1,9 +1,11 @@
 function tagline() {
+    const root = document.querySelector(':root') as HTMLElement;
     const closeTaglineBtn = document.querySelector('#taglineClose') as HTMLButtonElement;
     const taglineElement = document.querySelector('#tagline') as HTMLDivElement;
 
     closeTaglineBtn.onclick = () => {
         taglineElement.remove();
+        root.style.setProperty('--tagline-height', '0px');
     };
 }
 
